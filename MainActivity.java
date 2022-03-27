20378c4c1ee4a453aee2dba95c28c64859670b3e
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-03-27 22:52:25 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import androidx.appcompat.app.AlertDialog;
-import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
 
